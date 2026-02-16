0d837102943ad4bc1e22fd7da7fc9a8a44e61dc6
Rafael Menezes <rafa.rcm@hotmail.com>
2026-02-16 14:17:33 -0300

    integrando-tela-cadastro-book-api

--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import "./styles.css";
 import logoImage from "../../assets/logo.svg";
+import api from "../../services/api";
 
 export default function NewBook() {
+  const navigate = useNavigate();
+
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [launchDate, setLaunchDate] = useState("");
+  const [price, setPrice] = useState("");
+
+  async function createNewBook(e) {
+    e.preventDefault();
+
+    const accessToken = localStorage.getItem("accessToken");
+    const data = {
+      title,
+      author,
+      launchDate,
+      price,
+    };
+    try {
+      await api.post("/api/Book/v1", data, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+    } catch (err) {
+      alert("Error while recording Book! Try again");
+    }
+
+    navigate("/books");
+  }
+
   return (
     <div className="new-book-container">
       <div className="content">
@@ -17,11 +48,27 @@ export default function NewBook() {
             Home
           </Link>
         </section>
-        <form>
-          <input placeholder="Title" />
-          <input placeholder="Autor" />
-          <input type="date" />
-          <input placeholder="Price" />
+        <form onSubmit={createNewBook}>
+          <input
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <input
+            type="date"
+            value={launchDate}
+            onChange={(e) => setLaunchDate(e.target.value)}
+          />
+          <input
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
 
           <button className="button" type="submit">
             Add
